592d1812537caf1777945e29bc386bcf920b3085
Belleisa <47032848+Belleisa@users.noreply.github.com>
2020-05-22 17:28:38 -0300

    update app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { IndexComponent } from './index/index.component';
 import { GithubComponent } from './github/github.component';
 import { BitcoinComponent } from './bitcoin/bitcoin.component';
-
+import { DetailsComponent } from './details/details.component';
 
 const routes: Routes = [
   { path: '', component: IndexComponent },
   { path: 'github', component: GithubComponent },
   { path: 'bitcoin', component: BitcoinComponent },
+  { path: 'repos/:index', component: DetailsComponent}
 ];
 
 @NgModule({
